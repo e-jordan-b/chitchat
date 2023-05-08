0fb07d534912c237b745dacdd403edf072d73849
Federico <federico.b.giordani@gmail.com>
2023-05-08 14:42:15 +0200

    switching to features branches

---
+++ server/src/database/index.ts
@@ -0,0 +1,11 @@
+import mongoose, { MongooseError } from 'mongoose';
+
+// const connectDB = async (): Promise<{
+//   success: boolean;
+//   error?: MongooseError;
+// }> => {
+//   const user = process.env.MONGODB_USER;
+//   const pass = process.env.MONGODB_PASS;
+
+//   const uri
+// };
