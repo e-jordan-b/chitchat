d5544e18c5c977282db4e1c2b9f088afac39be6a
Leon Gieser <leon.gieser@gmail.com>
2023-05-10 17:40:43 +0200

    added custom tailwind w / h and setup dashboard layout

--- client/src/routes/Conversations.tsx
+++ client/src/routes/Conversations.tsx
@@ -1,49 +1,61 @@
 
-import { useNavigation } from 'react-router-dom'
-import { useState } from "react"
+import { useNavigation, useLoaderData } from 'react-router-dom'
+// import { useState } from "react"
+import { useQuery } from "@tanstack/react-query";
 
 
-
-import { useLoaderData } from 'react-router-dom'
-// import { getContact } from '../contacts'
-
-// ⬇️ this is the loader for the detail route
-export async function loader({ params }: { params: { contactId: string } }) {
-  // return getContact(params.contactId)
-  return  { id: 1, name: "John" }
+const getFakeData = () => {
+  // await new Promise((resolve) => setTimeout(resolve, 1000));
+  return [
+    { id: 1, started: "2020-05-09 03:22:58", ended: "2020-05-09 03:25:58",name: "John", body: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptate ullam esse voluptates, excepturi hic molestiae blanditiis ", summary: ["mimmimii", "mumumumum", "mamamamam", "memememem"]},
+    { id: 2, started: "2022-03-31 18:39:42", ended: "2022-03-31 18:59:42",name: "Jane", body: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptate ullam esse voluptates, excepturi hic molestiae blanditiis ", summary: ["mimmimii", "mumumumum", "mamamamam", "memememem"]},
+    { id: 3, started: "2021-02-01 05:28:01", ended: "2021-02-01 01:28:01",name: "Jack", body: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptate ullam esse voluptates, excepturi hic molestiae blanditiis ", summary: ["mimmimii", "mumumumum", "mamamamam", "memememem"]},
+    { id: 4, started: "2022-01-25 07:42:23", ended: "2022-01-25 08:22:23",name: "John", body: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptate ullam esse voluptates, excepturi hic molestiae blanditiis ", summary: ["mimmimii", "mumumumum", "mamamamam", "memememem"]},
+  ]
 }
+
 
 export type ConversationType = {
   id: string,
+  started: string,
+  ended: string,
   name: string,
   body: string
-
 }
 
 
-export default function Conversations() {
-  const conversations = useLoaderData() as ConversationType[]
-  const navigation = useNavigation();
-  // const [navigation, setNavigation] = useState({state: "loading"})
-  // const toggleNavigation = () => {
-  //   if (navigation.state === "loading") {
-  //     setNavigation({state: "loaded"})
-  //   } else {
-  //     setNavigation({state: "loading"})
-  //   }
-  // }
+export async function loader() {
+  const conversations = await getFakeData();
+  return { conversations };
+}
 
+// export const loader =
+//   (queryClient) =>
+//   async ({ params }) => {
+//     const query = contactDetailQuery(params.contactId);
+//     return (
+//       queryClient.getQueryData(query.queryKey) ??
+//       (await queryClient.fetchQuery(query))
+//     );
+//   };
+
+
+
+
+export default function Conversations() {
+  const navigation = useNavigation();
+  const {conversations} = useLoaderData() as {conversations: ConversationType[]}
 
   return (
     <div className={`flex flex-col justify-center items-center ${ navigation.state === "loading" ? "opacity-25 transition delay-200" : null}`}>
-    {/* <button onClick={toggleNavigation}>toggle Loading State</button> */}
     <div>Conversations</div>
     <div>
-      {conversations.map((conversation: ConversationType) => {
+      {conversations.map((conversation) => {
         return (
-          <div key={conversation.id}>
-            <div>{conversation.name}</div>
-            <div>{conversation.body}</div>
+          <div className="flex" key={conversation.id}>
+
+            <div className="mr-5">{conversation.name}</div>
+            <div className='truncate w-40'>{conversation.body}</div>
           </div>
         )
       })
--- client/src/routes/Dashboard.tsx
+++ client/src/routes/Dashboard.tsx
@@ -2,8 +2,28 @@ import { useLoaderData } from 'react-router-dom'
 
 export default function Dashboard() {
   return (
-    <div className='border-2 border-red-400 w-full h-full flex flex-col justify-center items-center'>
-      <div>Dashboard</div>
-    </div>
+    <div className='border-2 border-red-400 bg-red-200 w-full h-full flex flex-col justify-center items-center'>
+
+      <div className="flex justify-around items-center border border-zinc-700 w-11/12 h-9/10 rounded-lg bg-green-300">
+
+        <section className='flex flex-col items-center justify-center boder-2 bg-slate-500  border-zinc-600 w-2/5 h-4/6 rounded-xl'>
+
+             <button className='primary-btn'>Start a new Call</button>
+             <label htmlFor="join-call">join a call</label>
+             <input className="border border-zinc-700"type="text" />
+        </section>
+
+        <section className='flex flex-col items-center boder-2 bg-slate-500  border-zinc-600 w-2/5 h-4/6 rounded-xl'>
+
+            <h4>Previous Calls</h4>
+            <div className='bg-slate-500'>
+              TODO: add tanstack table + shadcn-ui
+            </div>
+
+        </section>
+
+       </div>
+      </div>
+
   )
 }
