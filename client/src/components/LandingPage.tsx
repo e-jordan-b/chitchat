2cb46fe1a55f3d7a43d3d2f7f76571feac063ec4
Èric Jordan Bernardo <ericjordan1998@gmail.com>
2023-05-11 15:53:17 +0200

    Added styling

--- client/src/components/LandingPage.tsx
+++ client/src/components/LandingPage.tsx
@@ -16,19 +16,33 @@ const LandingPage= () => {
   };
 
   return (
-    <div className="flex flex-row justify-center items-center h-screen ">
-        <div className="create box mr-10 ">
-            <button className="bg-slate-400 p-5" onClick={handleClick}>Create Call</button>
+    <div className="flex flex-col justify-center items-center h-screen w-screen">
+      <div className="w-1/4">
+        <div className='mb-5'>
+          <h1 className="text-6xl">Lorem ipsum dolor sit amet</h1>
+        </div>
+        <div>
+          <p className="text-gray-500">
+            Lorem ipsum dolor sit amet consectetur adipiscing elit sed molestie iaculis, gravida tempor vestibulum accumsan vitae nostra massa
+            gravida tempor vestibulum accumsan vitae nostra massa
+          </p>
+        </div>
+      </div>
+      <div className= "flex justify-between items-center w-1/4 mt-16">
+        <div className="text-white flex justify-center items-center">
+            <button className="bg-slate-600 px-7 py-4 " onClick={handleClick}>Create Call</button>
         </div>
 
-        <div className="answer box">
+        <div className="flex justify-center items-center border-2 border-gray-300 rounded">
             <input
+                className="px-2 py-4 text-left"
                 value={joinCode}
                 onChange={(e) => setJoinCode(e.target.value)}
                 placeholder="Join with code"
             />
-            <button onClick={handleClick}>Answer</button>
+            <button onClick={handleClick} className="flex justify-center items-center rounded-xl whitespace-nowrap mr-4 transition-all duration-150 h-14 ">Join Call</button>
         </div>
+      </div>
     </div>
   )
 };
