52cdda00b4472c0c691be4270c6f3ef069bfc7bb
Federico <federico.b.giordani@gmail.com>
2023-05-12 10:07:44 +0200

    feat: implementing room-component

--- client/src/components/room/room.tsx
+++ client/src/components/room/room.tsx
@@ -1,8 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './room.css';
 
+import RoomPreCall from './room-precall';
+import RoomCall from './room-call';
+
 const Room: React.FC = () => {
-  return <div className="room"></div>;
+  const [audioStream, setAudioStream] = useState<MediaStream>();
+  const [videoStream, setVideoStream] = useState<MediaStream>();
+  const [showPrecall, setShowPrecall] = useState<boolean>();
+
+  // Connection To Web
+  return (
+    <div className="room">{showPrecall ? <RoomPreCall /> : <RoomCall />}</div>
+  );
 };
 
 export default Room;
