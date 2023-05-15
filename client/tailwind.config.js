ea08ee70cf4906bb41c458fcdf2030c683feb661
Leonor Aidos <leonoraidos98@gmail.com>
2023-05-15 16:36:20 +0100

    Feat: landing Page

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -4,7 +4,26 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    scale: {
+      '30': '.30',
+      '30-percent': '0.30'
+    },
+    extend: {
+      colors: {
+        'custom-purple': {
+          "900": '#312E81',
+          '800': '#3730A3',
+          '700': '#4338CA',
+          "600": '#4F46E5',
+          '500': '#6366F1',
+          '400': '#818CF8',
+          "300": '#A5B4FC',
+          '200': '#C7D2FE',
+          '100': '#E0E7FF',
+          '50:': '#EEF2FF'
+        }
+      }
+    },
   },
   plugins: [],
 }
