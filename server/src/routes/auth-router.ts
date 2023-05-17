441e059c265bb2bb591e41dff39a2cc9f01c39c0
Leon Gieser <leon.gieser@gmail.com>
2023-05-17 17:40:41 +0200

    forgot to add server changes

--- server/src/routes/auth-router.ts
+++ server/src/routes/auth-router.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
-import { signin, signup } from '../controllers/auth-controller';
+import { signin, signup, signout } from '../controllers/auth-controller';
 
 const router = Router();
 
 router.post('/signin', signin);
 
 router.post('/signup', signup);
+
+router.post('/signout', signout)
 
 export { router as authRouter };
